1bb4e0316a1e8da9613e52f137253ff04430cf43
Maciej Trątnowiecki <mtratnowiecki@gmail.com>
2021-04-03 17:18:34 +0200

    Live-reload react app

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,4 +1,19 @@
+const path = require('path');
+const webpack = require('webpack');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const BundleTracker = require('webpack-bundle-tracker');
+
+
 module.exports = {
+   output: {
+    path: path.resolve(__dirname, "static/frontend"), // Should be in STATICFILES_DIRS
+    publicPath: "/static/", // Should match Django STATIC_URL
+    filename: "[name].js", // No filename hashing, Django takes care of this
+    chunkFilename: "[id]-[chunkhash].js", // DO have Webpack hash chunk filename, see below
+  },
+  devServer: {
+    writeToDisk: true, // Write files to disk in dev mode, so Django can serve the assets
+  },
   module: {
     rules: [
       {
@@ -9,6 +24,10 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new BundleTracker({filename: './webpack-stats.json'})
+  ]
 };
 
