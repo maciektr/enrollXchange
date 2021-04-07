1cdd72541879d269ac2f909e24c0b4d7d3ce769e
Jakub Solecki <jakub.solecki@onet.com.pl>
2021-04-07 20:06:23 +0200

    fixed script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,7 @@ else
   ( cd frontend ; yarn build )
 fi
 if [ "ENVIRONMENT" = "local" ]; then
- gunicorn -b 0.0.0.0:8000 enrollXchange.wsgi --reload --log-level "${GUNICORN_LOG_LEVEL}" &
+#  gunicorn -b 0.0.0.0:8000 enrollXchange.wsgi --reload --log-level "${GUNICORN_LOG_LEVEL}" &
   python manage.py runserver 0.0.0.0:8000 &
   python manage.py livereload
 else
