b7a94a911ca7f1d15cf7af3e696a0356dd2d13af
DenkoV <mr.denkov@gmail.com>
2021-04-08 12:16:41 +0200

    Add functional buttons

--- frontend/src/components/market/OfferElement.js
+++ frontend/src/components/market/OfferElement.js
@@ -1,7 +1,16 @@
-import React from 'react';
-import {Button} from "react-bootstrap";
+import React, {useState} from 'react';
+import {Button, Modal} from "react-bootstrap";
 
 const OfferElement = ({props}) => {
+    const [showInfo, setShowInfo] = useState(false);
+    const [showConfirmation, setShowConfirmation] = useState(false);
+
+    const handleConfirmation = () => {
+        //TODO handle exchange
+        setShowConfirmation(false)
+        alert("DONE")
+    }
+
     return (
         <div className="offset-2 col-8 offer-element d-flex justify-content-between align-items-center mt-4">
             <div className="ml-4">
@@ -13,8 +22,53 @@ const OfferElement = ({props}) => {
                     {props.day + ' ' + props.time}
                 </span>
                 <span className="ml-5">
-                    <Button variant="info" className="mr-3">Info</Button>
-                    <Button variant="success">Zaakceptuj</Button>
+                    <Button variant="info" className="mr-3" onClick = {() => setShowInfo(true)}>Info</Button>
+
+                    <Modal
+                    show={showInfo}
+                    onHide={() => setShowInfo(false)}
+                    size="lg"
+                    aria-labelledby="contained-modal-title-vcenter"
+                    centered>
+                    <Modal.Header closeButton>
+                        <Modal.Title id="contained-modal-title-vcenter">
+                        {props.title + ' - ' + props.lecturer + '\t' + props.day + ' '+props.time}
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <p>
+                        {props.comment}
+                        </p>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button onClick={() => setShowInfo(false)}>Zamknij</Button>
+                    </Modal.Footer>
+                    </Modal>
+
+
+                    <Button variant="success" onClick = {() => setShowConfirmation(true)}>Zaakceptuj</Button>
+
+                    <Modal
+                    show={showConfirmation}
+                    onHide={() => setShowConfirmation(false)}
+                    size="lg"
+                    aria-labelledby="contained-modal-title-vcenter"
+                    centered>
+                    <Modal.Header closeButton>
+                        <Modal.Title id="contained-modal-title-vcenter">
+                        {props.title + ' - ' + props.lecturer + '\t' + props.day + ' '+props.time}
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <h5>
+                        Czy potwierdzasz zamianę?
+                        </h5>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant='success' onClick={() => handleConfirmation()}>Zaakceptuj</Button>
+                        <Button variant='danger' onClick={() => setShowConfirmation(false)}>Odrzuć</Button>
+                    </Modal.Footer>
+                    </Modal>
                 </span>
             </div>
 
