459cdd48e33e004f8135524703014b029c4e653d
mozarek <karonkacper@gmail.com>
2021-04-08 12:37:52 +0200

    refactor component class to function

--- frontend/src/components/AddOffer/AddOfferForm.js
+++ frontend/src/components/AddOffer/AddOfferForm.js
@@ -24,60 +24,58 @@ function getDay(date) {
 }
 
 
-class AddOfferForm extends React.Component {
+const AddOfferForm = (props) => {
     // TODO: add loading data (can be even mock) and show in dropdowns
     // TODO: actively validate form so that user can not pick non-existent class
     // TODO: make it look better :)
-    render() {
-        return (
-            <Modal show={this.props.show} onHide={this.props.onHide}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Szczegóły terminu</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <Row><b>Twój termin:</b></Row>
-                    <Container className="offset-0.3">
-                        <Row>{getDay(this.props.event.start)} </Row>
-                        <Row>{getDateString(this.props.event.start)} - {getDateString(this.props.event.end)}</Row>
-                        <Row>{this.props.event.title}</Row>
-                    </Container>
+    return (
+        <Modal show={props.show} onHide={props.onHide}>
+            <Modal.Header closeButton>
+                <Modal.Title>Szczegóły terminu</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Row><b>Twój termin:</b></Row>
+                <Container className="offset-0.3">
+                    <Row>{getDay(props.event.start)} </Row>
+                    <Row>{getDateString(props.event.start)} - {getDateString(props.event.end)}</Row>
+                    <Row>{props.event.title}</Row>
+                </Container>
 
-                    <Row><b>Zamień swój termin:</b></Row>
-                    <Form>
-                        <Form.Group controlId="AddOfferForm.daySelect">
-                            <Form.Label>Preferowany dzień zajęć</Form.Label>
-                            <Form.Control as='select' custom>
-                                <option>dowolny</option>
-                                <option>Piątek</option>
-                            </Form.Control>
-                        </Form.Group>
-                        <Form.Group controlId="AddOfferForm.timeSelect">
-                            <Form.Label>Preferowany czas zajęć</Form.Label>
-                            <Form.Control as='select' custom>
-                                <option>dowolny</option>
-                                <option>8:00 - 9:30</option>
-                            </Form.Control>
-                        </Form.Group>
-                        <Form.Group controlId="AddOfferForm.teacherSelect">
-                            <Form.Label>Preferowany prowadzący</Form.Label>
-                            <Form.Control as='select' custom>
-                                <option>dowolny</option>
-                                <option>Piotr Faliszewski</option>
-                            </Form.Control>
-                        </Form.Group>
-                        <Form.Group controlId="AddOfferForm.commentTextArea">
-                            <Form.Label>Komentarz</Form.Label>
-                            <Form.Control as="textarea" rows={2} />
-                        </Form.Group>
-                    </Form>
-                    
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="danger">Złóż ofertę zamiany</Button>
-                </Modal.Footer>
-            </Modal>
-        )
-    }
+                <Row><b>Zamień swój termin:</b></Row>
+                <Form>
+                    <Form.Group controlId="AddOfferForm.daySelect">
+                        <Form.Label>Preferowany dzień zajęć</Form.Label>
+                        <Form.Control as='select' custom>
+                            <option>dowolny</option>
+                            <option>Piątek</option>
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId="AddOfferForm.timeSelect">
+                        <Form.Label>Preferowany czas zajęć</Form.Label>
+                        <Form.Control as='select' custom>
+                            <option>dowolny</option>
+                            <option>8:00 - 9:30</option>
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId="AddOfferForm.teacherSelect">
+                        <Form.Label>Preferowany prowadzący</Form.Label>
+                        <Form.Control as='select' custom>
+                            <option>dowolny</option>
+                            <option>Piotr Faliszewski</option>
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId="AddOfferForm.commentTextArea">
+                        <Form.Label>Komentarz</Form.Label>
+                        <Form.Control as="textarea" rows={2} />
+                    </Form.Group>
+                </Form>
+                
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="danger">Złóż ofertę zamiany</Button>
+            </Modal.Footer>
+        </Modal>
+    )
 }
 
 export default AddOfferForm;
