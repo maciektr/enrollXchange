acc6758f7e063402f4603363370118d40e2be019
mozarek <karonkacper@gmail.com>
2021-04-08 15:45:08 +0200

    fixed docker entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,11 @@
 python manage.py migrate
 ( cd frontend ; yarn install )
-if [ "ENVIRONMENT" = "local" ]; then
-  (cd frontend ; yarn dev ) &
+if [ "${ENVIRONMENT}" = "local" ]; then
+  ( cd frontend ; yarn dev ) &
 else
   ( cd frontend ; yarn build )
 fi
-if [ "ENVIRONMENT" = "local" ]; then
+if [ "${ENVIRONMENT}" = "local" ]; then
 #  gunicorn -b 0.0.0.0:8000 enrollXchange.wsgi --reload --log-level "${GUNICORN_LOG_LEVEL}" &
   python manage.py runserver 0.0.0.0:8000 &
   python manage.py livereload
