91b950c6aea3edee49067b28d8e58097f557fabc
Krzysztof Nalepa <misiek.nalepa@wp.pl>
2021-04-11 16:51:56 +0200

    offer text-color fixed

--- frontend/src/components/MySchedule/AddOfferForm.js
+++ frontend/src/components/MySchedule/AddOfferForm.js
@@ -25,9 +25,6 @@ function getDay(date) {
 
 
 const AddOfferForm = (props) => {
-    // TODO: add loading data (can be even mock) and show in dropdowns
-    // TODO: actively validate form so that user can not pick non-existent class
-    // TODO: make it look better :)
     return (
         <Modal show={props.show} onHide={props.onHide}>
             <Modal.Header closeButton>
@@ -40,38 +37,38 @@ const AddOfferForm = (props) => {
                     <div>{getDateString(props.event.start)} - {getDateString(props.event.end)}</div>
                     <div>{props.event.title}</div>
                 </Col>
-                <hr></hr>
+                <hr/>
                 <Col>
                     <b>Zamień swój termin:</b>
                     <Form>
                         <Form.Group controlId="AddOfferForm.daySelect">
-                            <Form.Label>Preferowany dzień zajęć</Form.Label>
+                            <Form.Label><span style={{color: 'black'}}>Preferowany dzień zajęć</span></Form.Label>
                             <Form.Control as='select' custom>
                                 <option>dowolny</option>
                                 <option>Piątek</option>
                             </Form.Control>
                         </Form.Group>
                         <Form.Group controlId="AddOfferForm.timeSelect">
-                            <Form.Label>Preferowany czas zajęć</Form.Label>
+                            <Form.Label><span style={{color: 'black'}}>Preferowany czas zajęć</span></Form.Label>
                             <Form.Control as='select' custom>
                                 <option>dowolny</option>
                                 <option>8:00 - 9:30</option>
                             </Form.Control>
                         </Form.Group>
                         <Form.Group controlId="AddOfferForm.teacherSelect">
-                            <Form.Label>Preferowany prowadzący</Form.Label>
+                            <Form.Label><span style={{color: 'black'}}>Preferowany prowadzący</span></Form.Label>
                             <Form.Control as='select' custom>
                                 <option>dowolny</option>
                                 <option>Piotr Faliszewski</option>
                             </Form.Control>
                         </Form.Group>
                         <Form.Group controlId="AddOfferForm.commentTextArea">
-                            <Form.Label>Komentarz</Form.Label>
+                            <Form.Label><span style={{color: 'black'}}>Komentarz</span></Form.Label>
                             <Form.Control as="textarea" rows={2} />
                         </Form.Group>
                     </Form>
                 </Col>
-                
+
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="danger">Złóż ofertę zamiany</Button>
