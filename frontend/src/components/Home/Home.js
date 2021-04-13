09e050b614006d04af1c5f53785726729af0037f
Krzysztof Nalepa <misiek.nalepa@wp.pl>
2021-04-13 20:28:58 +0200

    deleted unnecessary pages

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,15 +1,16 @@
 import React from "react";
-import {Button, Col, Container, Row} from "react-bootstrap";
+import { Col, Container, Row} from "react-bootstrap";
 import {Link} from "react-router-dom";
-import '../../styles/common.css'
-import {userContext} from '../../context/userContext'
+import {userContext} from '../../context/userContext';
+import '../../styles/home.css';
+
 const Home = () => {
     return (
         <div className="bg-image">
             <Container className="image-container" >
                 <Row className="row-login">
                     <Col className="col-6 offset-3 col-login">
-                        <div className="text-center" id="title">enrollXchange</div>
+                        <div className="text-center title">enrollXchange</div>
                         <div className="mt-5 d-flex flex-column justify-content-around">
 
                             <userContext.Consumer>
