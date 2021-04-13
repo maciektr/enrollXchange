3a8ccee6a3b0f9b023eb37a477a625c887dd3fcc
Krzysztof Nalepa <misiek.nalepa@wp.pl>
2021-04-13 21:49:38 +0200

    homepage working

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, {useContext} from "react";
 import { Col, Container, Row} from "react-bootstrap";
 import '../../styles/home.css';
+import {UserContext} from "../../context/User";
+import {Link} from "react-router-dom";
 
 const Home = () => {
+    const { user, _setUser } = useContext(UserContext);
     return (
         <div className="bg-image">
             <Container className="image-container" >
@@ -10,30 +13,24 @@ const Home = () => {
                     <Col className="col-6 offset-3 col-login">
                         <div className="text-center title">enrollXchange</div>
                         <div className="mt-5 d-flex flex-column justify-content-around">
-
-                            {/*<userContext.Consumer>*/}
-                            {/*    {(user) => (*/}
-                            {/*        <div>*/}
-                            {/*            {!user ?*/}
-                            {/*                (<a className="btn btn-outline-light" style={{width: '100%'}}*/}
-                            {/*                   href="/accounts/login">Zaloguj</a>)*/}
-                            {/*                :*/}
-                            {/*                (*/}
-                            {/*                    <div className="text-center">*/}
-                            {/*                        <h2 style={{color: "white"}}>Witaj {user.username}!</h2>*/}
-                            {/*                        <Link to="/my-schedule">*/}
-                            {/*                            <button className="btn btn-outline-light mt-3" style={{width: '100%'}}>Mój podział</button>*/}
-                            {/*                        </Link>*/}
-                            {/*                        <Link to="/market">*/}
-                            {/*                            <button className="btn btn-outline-light mt-5" style={{width: '100%'}}>Giełda</button>*/}
-                            {/*                        </Link>*/}
-                            {/*                        <a className="btn btn-outline-light mt-5" style={{width: '100%'}}*/}
-                            {/*                           href="/accounts/logout">Wyloguj</a>*/}
-                            {/*                    </div>*/}
-                            {/*                )*/}
-                            {/*            }*/}
-                            {/*        </div>)}*/}
-                            {/*</userContext.Consumer>*/}
+                            {!user ?
+                                (
+                                    <a className="btn btn-outline-light" style={{width: '100%'}}
+                                    href="/accounts/login">Zaloguj</a>)
+                                : (
+                                    <div className="text-center">
+                                        <h2 style={{color: "white"}}>Witaj {user.username}!</h2>
+                                        <Link to="/my-schedule">
+                                            <button className="btn btn-outline-light mt-3" style={{width: '100%'}}>Mój podział</button>
+                                        </Link>
+                                        <Link to="/market">
+                                            <button className="btn btn-outline-light mt-5" style={{width: '100%'}}>Giełda</button>
+                                        </Link>
+                                        <a className="btn btn-outline-light mt-5" style={{width: '100%'}}
+                                           href="/accounts/logout">Wyloguj</a>
+                                    </div>
+                                )
+                            }
                         </div>
                     </Col>
 
