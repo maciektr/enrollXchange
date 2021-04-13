127f98addd4a385696d8e03a590ab7e5397902cd
mozarek <karonkacper@gmail.com>
2021-04-14 01:02:57 +0200

    working filtering!

--- enroll/schema/schema.py
+++ enroll/schema/schema.py
@@ -3,8 +3,8 @@ from graphene import relay
 from graphql_auth.schema import UserQuery, MeQuery
 from graphene_django.filter import DjangoFilterConnectionField
 
-from .types import CourseType, OfferType
-from ..models import Offer
+from .types import CourseType, OfferType, ClassTimeType
+from ..models import Offer, ClassTime
 
 
 class CourseConnection(relay.Connection):
@@ -15,11 +15,18 @@ class CourseConnection(relay.Connection):
 class Query(MeQuery, graphene.ObjectType):
     courses = relay.ConnectionField(CourseConnection, resolver=CourseType.resolve_all)
     offers = DjangoFilterConnectionField(OfferType)
+    class_times = DjangoFilterConnectionField(ClassTimeType)
 
     def resolve_offers(self, info, **kwargs):
         if info.context.user.is_authenticated:
             return Offer.objects.all()
         return Offer.objects.none()
+    
+    def resolve_class_times(self, info, **kwargs):
+        if info.context.user.is_authenticated:
+            return ClassTime.objects.all()
+        return ClassTime.objects.none()
+
 
 # class MyMutations(graphene.ObjectType):
 #     pass
--- enroll/schema/types.py
+++ enroll/schema/types.py
@@ -40,6 +40,13 @@ class ClassTimeType(DjangoObjectType):
         model = ClassTime
         interfaces = (relay.Node,)
         fields = "__all__"
+        filter_fields = [
+            "day",
+            "frequency",
+            "start",
+            "lecturer__first_name",
+            "lecturer__last_name"
+        ]
 
     @staticmethod
     def resolve_all(root, info, **kwargs):
@@ -65,5 +72,9 @@ class OfferType(DjangoObjectType):
         filter_fields = [
             'active',
             'enrollment__class_time__course__full_name',
-            'enrollment__class_time__lecturer__full_name'
+            'enrollment__class_time__lecturer__first_name',
+            'enrollment__class_time__lecturer__last_name',
+            'enrollment__class_time__day',
+            'enrollment__class_time__start',
+            'enrollment__class_time__frequency',
         ]
