c2fd94dcfa6b8bc5ea6dc7d835c16d91104ab40b
Jakub Solecki <jakub.solecki@onet.com.pl>
2021-04-26 01:32:06 +0200

    query works; full customization WIP

--- enroll/schema/schema.py
+++ enroll/schema/schema.py
@@ -2,6 +2,7 @@ import graphene
 from graphene import relay
 from graphql_auth.schema import UserQuery, MeQuery
 from graphene_django.filter import DjangoFilterConnectionField
+from django.db.models import Q
 
 from .types import CourseType, OfferType, ClassTimeType, EnrollmentType
 from ..models import Offer, ClassTime, Enrollment
@@ -17,17 +18,43 @@ class Query(MeQuery, graphene.ObjectType):
     offers = DjangoFilterConnectionField(OfferType)
     class_times = DjangoFilterConnectionField(ClassTimeType)
     enrollments = DjangoFilterConnectionField(EnrollmentType)
+    matchingOffers = DjangoFilterConnectionField(OfferType)
 
+
+    @staticmethod
     def resolve_enrollments(self, info, **kwargs):
         if info.context.user.is_authenticated:
             return Enrollment.objects.all()
         return Enrollment.objects.none()
 
+    @staticmethod
     def resolve_offers(self, info, **kwargs):
         if info.context.user.is_authenticated:
             return Offer.objects.all()
         return Offer.objects.none()
 
+    @staticmethod
+    def resolve_matchingOffers(self, info, **kwargs):
+        if info.context.user.is_authenticated:
+            user = info.context.user
+            user_enrollments = list(Enrollment.objects.filter(student=user))
+            user_class_times = [e.class_time for e in user_enrollments]
+            class_time_fields = [(c.day, c.frequency, c.start, c.end) for c in user_class_times]
+            query_list = [
+                Q(enrollment__class_time__day=fields[0]) & Q(enrollment__class_time__frequency=fields[1]) #&
+                # (Q(enrollment__class_time__start__gte=fields[3]) | Q(enrollment__class_time__start__lse=fields[2]))
+                for fields in class_time_fields
+            ]
+            final_query = Q()
+
+            for q in query_list:
+                final_query |= q
+
+            return Offer.objects.filter(~final_query)
+            # return Offer.objects.all()
+        return Offer.objects.none()
+
+    @staticmethod
     def resolve_class_times(self, info, **kwargs):
         if info.context.user.is_authenticated:
             return ClassTime.objects.all()
