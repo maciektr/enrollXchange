8b897eb96865b728d11a7079460ee9af246f9f76
Kacper Karoń <44933289+Mozarek@users.noreply.github.com>
2021-05-02 12:18:56 +0200

    Create offer any (#22)
    * initial filters
    
    * working filtering!
    
    * Update schema.py
    
    whitespace removal service.
    
    * mutation for adding offer and enrollments query
    
    * Add schedule filter by base64encoded student:id
    
    * Add classTimes filter by course code or course name
    
    * Format
    
    * Filters (#19)
    
    * basic filter working
    
    * filter working yaay
    
    * proper fetching done
    
    * querries
    
    * lecturer filter added
    
    * filters done
    
    * query fix
    
    * custom schedule added
    
    * my offers done
    
    * my offers done
    
    * adding offers done
    
    * backend  for creating offer with any working
    
    * refactor
    
    * refactor2
    
    Co-authored-by: MrDenkoV <47295546+MrDenkoV@users.noreply.github.com>
    Co-authored-by: DenkoV <mr.denkov@gmail.com>
    Co-authored-by: Krzysztof Nalepa <56135216+kraleppa@users.noreply.github.com>

--- enroll/schema/schema.py
+++ enroll/schema/schema.py
@@ -34,6 +34,64 @@ class Query(MeQuery, graphene.ObjectType):
         return ClassTime.objects.none()
 
 
+class CreateOfferWithAny(graphene.Mutation):
+    offer = graphene.Field(OfferType)
+
+    class Arguments:
+        enrollment_id = graphene.String()
+        comment = graphene.String()
+        lecturer_id = graphene.String()
+        day = graphene.String()
+        frequency = graphene.String()
+        start = graphene.String()
+        duration = graphene.Int()
+
+    # on front end you can specify only those fields you ask for.
+    # The rest is automatically set to 'any'
+
+    @staticmethod
+    def mutate(
+        root,
+        info,
+        enrollment_id,
+        comment="",
+        lecturer_id=None,
+        day=None,
+        frequency=None,
+        start=None,
+        duration=None
+    ):
+        _, enrollment_id_real = relay.Node.from_global_id(global_id=enrollment_id)
+        enrollment = Enrollment.objects.get(id=enrollment_id_real)
+
+        class_times = ClassTime.objects.filter(course=enrollment.class_time.course)
+        if lecturer_id is not None:
+            _, lecturer_id_real = relay.Node.from_global_id(global_id=lecturer_id)
+            class_times = class_times.filter(lecturer__id=lecturer_id_real)
+        if day is not None:
+            class_times = class_times.filter(day=day)
+        if frequency is not None:
+            class_times = class_times.filter(frequency=frequency)
+        if start is not None:
+            class_times = class_times.filter(start=start)
+        if duration is not None:
+            class_times = class_times.filter(duration_minutes=duration)
+
+        try:
+            offer = Offer.objects.get(enrollment=enrollment)
+        except Offer.DoesNotExist as e:
+            offer = Offer.objects.create(
+                enrollment=enrollment,
+                comment=comment,
+                active=True
+            )
+
+        for class_time in class_times:
+            offer.exchange_to.add(class_time)
+
+        return CreateOffer(offer=offer)
+
+
 class CreateOffer(graphene.Mutation):
     offer = graphene.Field(OfferType)
 
@@ -53,24 +111,20 @@ class CreateOffer(graphene.Mutation):
         try:
             offer = Offer.objects.get(enrollment=enrollment)
         except Offer.DoesNotExist as e:
-            offer = None
-
-        if offer is not None:
-            offer.exchange_to.add(class_time)
-        else:
             offer = Offer.objects.create(
                 enrollment=enrollment,
                 comment=comment,
                 active=True
             )
-            offer.exchange_to.add(class_time)
+
+        offer.exchange_to.add(class_time)
 
         return CreateOffer(offer=offer)
 
 
 class MyMutations(graphene.ObjectType):
-    pass
     create_offer = CreateOffer.Field()
+    create_offer_with_any = CreateOfferWithAny.Field()
 
 
 schema = graphene.Schema(query=Query, mutation=MyMutations)
